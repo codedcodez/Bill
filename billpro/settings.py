f6026a4f0c4f3bbf1eaa8aee419835e264d584ac
John Tovor <johntovor@gmail.com>
2019-12-21 19:23:33 +0000

    local settings code added

--- billpro/settings.py
+++ billpro/settings.py
@@ -136,3 +136,9 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'billioncrypt@gmail.com'
 EMAIL_HOST_PASSWORD = 'hwmljptkvkbtejmk'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
