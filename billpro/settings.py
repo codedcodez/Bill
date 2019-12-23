e9a4f08bcd2c875888a39adc1608dfccc5499e0a
John Tovor <johntovor@gmail.com>
2019-12-23 11:02:36 +0000

    Change

--- billpro/settings.py
+++ billpro/settings.py
@@ -1,5 +1,4 @@
 import os
-from secret_details import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,8 +134,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+EMAIL_HOST_USER =
+EMAIL_HOST_PASSWORD =
 
 
 try:
